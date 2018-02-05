5c621938e120e7ee0a24f195dc9cd2eef6a12f47
Harrison Milao <harrison.milao@hotmail.com>
2018-02-04 23:06:56 -0200

    commit inicial

---
+++ js/type.js
@@ -0,0 +1,10 @@
+$(function(){
+
+var options = {
+  strings: ["somos a fivecode.","&amp; criamos solu&ccedil;&otilde;es para a transforma&ccedil;&atilde;o digital da sua empresa. "],
+  typeSpeed: 50
+}
+
+var typed = new Typed(".write", options);
+
+});
